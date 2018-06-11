bf33b170d92d763217ada53e68df29c596838439
zfowed <zfowed@qq.com>
2018-06-11 23:08:28 +0800

    异步加载

--- src/plugins/vue-fontawesome.js
+++ src/plugins/vue-fontawesome.js
@@ -1,17 +1,29 @@
-import fontawesome from '@fortawesome/fontawesome';
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
-import solid from '@fortawesome/fontawesome-free-solid';
-// import regular from '@fortawesome/fontawesome-free-regular';
-// import brands from '@fortawesome/fontawesome-free-brands';
 
-fontawesome.library.add(solid);
-// fontawesome.library.add(regular);
-// fontawesome.library.add(brands);
+function component() {
+  return Promise.all([
+    import(/* webpackChunkName: 'vue-fontawesome' */ '@fortawesome/fontawesome'),
+    import(/* webpackChunkName: 'vue-fontawesome' */ '@fortawesome/vue-fontawesome'),
+    import(/* webpackChunkName: 'vue-fontawesome' */ '@fortawesome/fontawesome-free-solid'),
+    // import(/* webpackChunkName: 'vue-fontawesome' */ '@fortawesome/fontawesome-free-regular'),
+    // import(/* webpackChunkName: 'vue-fontawesome' */ '@fortawesome/fontawesome-free-brands'),
+  ]).then(([
+    { default: fontawesome },
+    { default: FontAwesomeIcon },
+    { default: solid },
+    // { default: regular },
+    // { default: brands },
+  ]) => {
+    fontawesome.library.add(solid);
+    // fontawesome.library.add(regular);
+    // fontawesome.library.add(brands);
+    return FontAwesomeIcon;
+  });
+}
 
 export default {
 
   install(Vue) {
-    Vue.component('font-awesome-icon', FontAwesomeIcon);
+    Vue.component('font-awesome-icon', component);
   },
 
 };
