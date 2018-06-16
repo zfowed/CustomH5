3da3a589a4ff017290ae9489992c32e7eece521c
lemon-lllllll <914293306@qq.com>
2018-06-16 11:51:35 +0800

    添加图标

--- src/plugins/vue-fontawesome/solid.js
+++ src/plugins/vue-fontawesome/solid.js
@@ -3,6 +3,9 @@ import fontawesome from '@fortawesome/fontawesome';
 
 import home from '@fortawesome/fontawesome-free-solid/faHome';
 
+import plus from '@fortawesome/fontawesome-free-solid/faPlus';
+
 fontawesome.library.add(
   home,
+  plus,
 );
